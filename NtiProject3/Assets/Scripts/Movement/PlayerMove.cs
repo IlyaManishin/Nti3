27b6431bacaa42bd93e94b9c9095f6135551bf4a
Danila <dobrohotovdana@gmail.com>
2022-12-07 15:36:59 +0700

    Add: -Interactive module (RE) -Sprint for main character Small changes...

--- NtiProject3/Assets/Scripts/Movement/PlayerMove.cs
+++ NtiProject3/Assets/Scripts/Movement/PlayerMove.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
 public class PlayerMove : MonoBehaviour {
     private Rigidbody2D _rigidbody2D;
     private Controls _playerInput;
+    private bool _isSprinting = false;
 
     [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _sprintSpeed;
 
+    public void OnSprint(InputAction.CallbackContext context) => _isSprinting = context.ReadValueAsButton();
 
     private void Awake() {
         _rigidbody2D = GetComponent<Rigidbody2D>();
@@ -22,6 +26,7 @@ public class PlayerMove : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        _rigidbody2D.velocity = _playerInput.Player.Move.ReadValue<Vector2>() * _moveSpeed * Time.fixedDeltaTime;
+        _rigidbody2D.velocity = _playerInput.Player.Move.ReadValue<Vector2>() * (_isSprinting? _sprintSpeed : _moveSpeed) * Time.fixedDeltaTime;
     }
+
 }
